3ec1b9c3c7e209b1d4f54a7959a5b7bfe2f4e22b
DESKTOP-UQLAUR6\Centhil <centhil95@outlook.com>
2018-08-26 11:58:42 +0530

    routes file added to segregate based topic

--- index.js
+++ index.js
@@ -6,36 +6,15 @@ const express = require('express'),
 const hostname = 'localhost';
 const port = 3000;
 
+const dishRouter = require('./routes/dishRouter');
+
 const app = express();
 
 app.use(morgan('dev'));
 
-app.use(bodyParser.json());
+app.use('/dishes', dishRouter);
 
-app.all('/dishes', (req,res,next) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  next();
-});
-
-app.get('/dishes', (req,res,next) => {
-    res.end('Will send all the dishes to you!');
-});
-
-app.post('/dishes', (req, res, next) => {
- res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
-});
-
-app.put('/dishes', (req, res, next) => {
-  res.statusCode = 403;
-  res.end('PUT operation not supported on /dishes');
-});
- 
-app.delete('/dishes', (req, res, next) => {
-    res.end('Deleting all dishes');
-});
-
-app.get('/dishes/:dishId', (req,res,next) => {
+/*app.get('/dishes/:dishId', (req,res,next) => {
     res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
 });
 
@@ -52,7 +31,7 @@ app.put('/dishes/:dishId', (req, res, next) => {
 
 app.delete('/dishes/:dishId', (req, res, next) => {
     res.end('Deleting dish: ' + req.params.dishId);
-});
+});*/
 
 app.use(express.static(__dirname + '/public'));
 
