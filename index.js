be8286ca3dbd7ee6c428676b27e4fb2880e5c505
DESKTOP-UQLAUR6\Centhil <centhil95@outlook.com>
2018-08-29 10:22:05 +0530

    Mongoose Part 3

--- index.js
+++ index.js
@@ -9,20 +9,29 @@ connect.then((db) => {
 
     console.log('Connected correctly to server');
     Dishes.create({
-        name: 'te',
-        description: 'Test'
+        name: 'Uthappizza',
+        description: 'test'
     })
     .then((dish) => {
         console.log(dish);
-        
+
         return Dishes.findByIdAndUpdate(dish._id, {
-                $set: {
-                    description: 'Updated Test'
-                }
-            }, {
-                new: true
-            })
-            .exec();
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
     })
     .then((dish) => {
         console.log(dish);
